809926375be68efaac78608cfa332ab2260aef16
ruisuiwnl <e.luis.pelaez@gmail.com>
2025-10-04 01:27:26 +0800

    configured modals and fixed minor modal design issues

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,10 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
+Alpine.store('ui', {
+    showPayment: false,
+    showUnit: false,
+    showTenantAssign: false,
+});
+
 Alpine.start();
