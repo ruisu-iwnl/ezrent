23046f1518365c7bbb8f3c4d1d351e7850205fd7
ruisuiwnl <e.luis.pelaez@gmail.com>
2025-10-03 20:46:42 +0800

    initiated laravel with blade

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Landing Page') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h1 class="text-2xl font-bold mb-2">Hello, world!</h1>
+                    <p>EZRent landing page.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
