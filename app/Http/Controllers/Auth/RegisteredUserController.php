33d6210cf095333f419356ebb2bc4ea7465dcc0c
ruisuiwnl <e.luis.pelaez@gmail.com>
2025-10-03 21:55:26 +0800

    register to tenant by default

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Tenant;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -40,6 +41,10 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $user->role = 'tenant';
+        $user->save();
+        Tenant::firstOrCreate(['user_id' => $user->id]);
 
         event(new Registered($user));
 
