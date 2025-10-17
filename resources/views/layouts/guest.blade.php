16e141ad55566b533e105a585eea9b624a518c83
ruisuiwnl <e.luis.pelaez@gmail.com>
2025-10-17 17:12:05 +0800

    logo and title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'easyrent') }}</title>
+
+        <link rel="icon" type="image/png" href="{{ asset('images/logos/logo2.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/reports/financial.blade.php
+++ resources/views/reports/financial.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Financial Report - {{ $currentMonth }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logos/logo2.png') }}"> 
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         @media print {
